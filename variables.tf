e014d707275f77e050709c994ead8f3f27140d15
rodrigomariusso-devops <rodrigomariussodevops@gmail.com>
2024-10-22 15:44:36 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-east-1"
+  default     = ""
 }
 
 variable "family" {
   description = "The family of your ECS task"
-  default     = "teste-tf"
+  default     = ""
 }
 
 variable "cpu" {
   description = "The number of CPU units used by the task"
-  default     = "1024"
+  default     = ""
 }
 
 variable "memory" {
   description = "The amount of memory (in MiB) used by the task"
-  default     = "1024"
+  default     = ""
 }
 
 variable "network_mode" {
   description = "The Docker networking mode to use for the containers in the task"
-  default     = "default"
+  default     = ""
 }
 
 variable "requires_compatibilities" {
@@ -31,27 +31,27 @@ variable "requires_compatibilities" {
 
 variable "execution_role_arn" {
   description = "The ARN of the task execution role"
-  default     = "arn:aws:iam::598723879413:role/ecsTaskExecutionRole"
+  default     = ""
 }
 
 variable "task_role_arn" {
   description = "The ARN of the IAM role that the task can assume"
-  default     = "arn:aws:iam::598723879413:role/ecsTaskExecutionRole"
+  default     = ""
 }
 
 variable "container_name" {
   description = "The name of the container"
-  default     = "teste-tf"
+  default     = ""
 }
 
 variable "container_image" {
   description = "The Docker image to use for the container"
-  default     = "teste:latest"
+  default     = ""
 }
 
 variable "container_port" {
   description = "The port number on the container"
-  default     = 80
+  default     = 
 }
 
 variable "host_port" {
